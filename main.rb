0e77e0d08ab49c99a67127b22519fc63a86ab1ac
Antonio Vivace <puma95.95@gmail.com>
2017-02-21 21:49:58 +0100

    Test case for equals

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 # require "rgfa"
 require_relative "/home/avivace/github/rgfa/lib/rgfa.rb"
+require "test/unit"
+include Test::Unit::Assertions
 
 graph1 = RGFA.from_file("/home/avivace/github/bio-p/ex1.gfa")
 graph2 = RGFA.from_file("/home/avivace/github/bio-p/ex2.gfa")
@@ -9,3 +11,25 @@ puts graph1.equals(graph2)
 puts graph1.equals(graph3)
 puts graph2.equals(graph3)
 puts graph3.equals(graph1)
+
+gfa1 = RGFA.new
+gfa2 = RGFA.new
+
+lines = ["H\tVN:Z:1.0",
+		 "S\t1\tAAAA",
+		 "S\t2\tBBBB",
+		 "S\t3\tCCCC",
+		 "L\t2\t+\t3\t+\t0M",
+		 "L\t2\t+\t1\t+\t0M"]
+lines.each {|l| gfa1 << l}
+
+lines2 = ["H\tVN:Z:1.0",
+		 "S\t2\tBBBB",
+		 "S\t3\tCCCC",
+		 "S\t1\tAAAA",
+		 "L\t2\t+\t1\t+\t0M",
+		 "L\t2\t+\t3\t+\t0M"]
+lines2.each {|l| gfa2 << l}
+
+puts gfa1.equals(gfa2)
+assert_equal(gfa1.equals(gfa2), true)
